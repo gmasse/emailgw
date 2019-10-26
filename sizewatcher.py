66abe19dbf58c89d438fc85d536056e824f5acb2
Germain Masse <g-dev@masse.me>
2019-10-26 19:21:57 +0000

    Minimal working script

--- sizewatcher.py
+++ sizewatcher.py
@@ -23,11 +23,40 @@ if os.path.exists(config_file):
 else:
     raise FileNotFoundError("Configuration file not found")
 
-conn = http.client.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-  urllib.parse.urlencode({
-    "token": PUSHOVER_TOKEN,
-    "user": PUSHOVER_USER,
-    "message": "hello world",
-  }), { "Content-type": "application/x-www-form-urlencoded" })
-conn.getresponse()
+data_percent = None
+metadata_percent = None
+
+result = subprocess.run(
+        [ 'lvs', 'vg-mail/thinpool', '--options=lv_size,lv_metadata_size,data_percent,metadata_percent', '--reportformat=json', '--units=g' ],
+        stdout=subprocess.PIPE)
+data = json.loads(result.stdout)
+
+if 'report' in data:
+    if len(data['report']) == 1:
+        if 'lv' in data['report'][0]:
+            if len(data['report'][0]['lv']) == 1:
+                data_percent = float(data['report'][0]['lv'][0]['data_percent'])
+                metadata_percent = float(data['report'][0]['lv'][0]['metadata_percent'])
+
+if data_percent is None or metadata_percent is None:
+    # Cannot retrieve value!
+    print('Cannot retrieve value!')
+else:
+    message = ''
+    if data_percent > 80:
+        message = message + 'data: {}% '.format(data_percent)
+    if metadata_percent > 80:
+        message = message + 'meta: {}% '.format(data_percent)
+
+    if len(message) > 0:
+        message = 'ALERT ' + message
+        conn = http.client.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json",
+          urllib.parse.urlencode({
+            "token": PUSHOVER_TOKEN,
+            "user": PUSHOVER_USER,
+            "message": message,
+          }), { "Content-type": "application/x-www-form-urlencoded" })
+        conn.getresponse()
+    else:
+        print('Everything is good!')
